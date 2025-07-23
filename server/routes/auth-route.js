5691c96a5cf771209fcdd14b6af5535e4d2d1699
Shafayet Nur Alam <30376236+shafayetnuralam@users.noreply.github.com>
2025-07-23 21:49:22 +0600

    Remove unused files and dependencies from undefsafe package; update server structure to use express.Router for modular routing.

---
+++ server/routes/auth-route.js
@@ -0,0 +1,22 @@
+// *----------------------
+// * express.Router
+// *----------------------
+
+//? In Express.js, express.Router() is a mini Express application without all the server configurations but with the ability to define routes, middleware, and even have its own set of route handlers. It allows you to modularize your routes and middleware to keep your code organized and maintainable.
+//* <https://expressjs.com/en/guide/routing.html>
+//? Use the express.Router class to create modular, mountable route handlers. A Router instance is a complete middleware and routing system; for this reason, it is often referred to as a “mini-app”.
+
+const express = require("express");
+const router = express.Router();
+
+// Route: GET /
+router.route("/").get((req, res) => {
+  res.status(200).send("Welcome MERN Series");
+});
+
+// Route: GET /register
+router.route("/register").get((req, res) => {
+  res.status(200).json({ msg: "Registration successful from router" });
+});
+
+module.exports = router; // ✅ Correct export
--- server/server.js
+++ server/server.js
@@ -1,19 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const router = require("./routes/auth-route");
 
-
-app.get(
-    "/",
-    (req, res) => {
-        res.status(200).send("Hello, World!");
-    }
-);
-
-app.get("/register", (req, res) => {
-    res.status(200).send("Register Page");
-});
+// Mount the Router: To use the router in your main Express app, you can "mount" it at a specific URL prefix
+app.use("/api/auth", router);
 
 const PORT = 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`server is running at port: ${PORT}`);
 });
